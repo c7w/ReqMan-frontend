90985d75c6cb272911c26869b19b60cc904cbad7
HBX-hbx <1136209374@qq.com>
2022-03-17 22:52:21 +0800

    [SR.002.107.I] refactor functional components to class components (#16)

--- src/layout/Home.tsx
+++ src/layout/Home.tsx
@@ -2,28 +2,31 @@ import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
 import { Layout, Breadcrumb } from "antd";
 import "./Home.css";
+import React from "react";
 
 const { Content, Footer } = Layout;
 
-const Home = () => {
-  return (
-    <div>
-      <Layout className="home">
-        <Sidebar />
-        <Layout className="layout">
-          <Navbar />
-          <Content className="content">
-            <Breadcrumb style={{ margin: "16px 0" }}>
-              <Breadcrumb.Item>User</Breadcrumb.Item>
-              <Breadcrumb.Item>hbx</Breadcrumb.Item>
-            </Breadcrumb>
-            <div style={{ padding: 24, minHeight: 360 }}>HI MAN!</div>
-          </Content>
-          <Footer className="home-background footer">FOOTER!</Footer>
+class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        <Layout className="home">
+          <Sidebar />
+          <Layout className="layout">
+            <Navbar />
+            <Content className="content">
+              <Breadcrumb style={{ margin: "16px 0" }}>
+                <Breadcrumb.Item>User</Breadcrumb.Item>
+                <Breadcrumb.Item>hbx</Breadcrumb.Item>
+              </Breadcrumb>
+              <div style={{ padding: 24, minHeight: 360 }}>HI MAN!</div>
+            </Content>
+            <Footer className="home-background footer">FOOTER!</Footer>
+          </Layout>
         </Layout>
-      </Layout>
-    </div>
-  );
-};
+      </div>
+    );
+  }
+}
 
 export default Home;
