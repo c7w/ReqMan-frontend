2ba9499e11a45be46b4d979595cce0242b611369
何秉翔 <2020010944@secoder.net>
2022-03-21 19:18:46 +0800

    [SR.002.108.I] Refactor the SRCard component. (#17)

--- src/components/rms/SRCard.tsx
+++ src/components/rms/SRCard.tsx
@@ -9,38 +9,37 @@ interface SRCardProps {
   readonly title: string; // title
   readonly description: string; // description
   readonly priority: number; // the priority which indicates the importance of the SR
-  readonly state: string; // "TODO", "WIP", "Reviewing", "Done"
+  readonly currState: string; // "TODO", "WIP", "Reviewing", "Done"
   readonly createdBy: string; // somebody
   readonly createdAt: number; // sometime
   readonly disabled: boolean;
 }
 
-class SRCard extends React.Component<SRCardProps> {
-  // default props for SRCard
-  static defaultProps = {
-    id: 0, // id
-    project: "test", // the project belongs to
-    title: "SR.002.103", // title
-    description: "hbx is working hard !", // description
-    priority: 0, // the priority which indicates the importance of the SR
-    state: "TODO", // "TODO", "WIP", "Reviewing", "Done"
-    createdBy: "hbx", // somebody
-    createdAt: new Date().getDate(), // sometime
-    disabled: false,
-  };
-  render() {
-    return (
-      <div className="card-small">
-        <div className="card-small-up">{this.props.title}</div>
-        <div className="card-small-description">
-          <Typography>
-            <Text>{this.props.description}</Text>
-          </Typography>
-        </div>
-        <div className="card-small-down"></div>
+const SRCard = (props: SRCardProps) => {
+  return (
+    <div className="card-small">
+      <div className="card-small-up">{props.title}</div>
+      <div className="card-small-description">
+        <Typography>
+          <Text>{props.description}</Text>
+        </Typography>
       </div>
-    );
-  }
-}
+      <div className="card-small-down"></div>
+    </div>
+  );
+};
+
+// default props for SRCard
+SRCard.defaultProps = {
+  id: 0, // id
+  project: "test", // the project belongs to
+  title: "SR.002.103", // title
+  description: "hbx is working hard !", // description
+  priority: 0, // the priority which indicates the importance of the SR
+  currState: "TODO", // "TODO", "WIP", "Reviewing", "Done"
+  createdBy: "hbx", // somebody
+  createdAt: new Date().getDate(), // sometime
+  disabled: false,
+};
 
 export default SRCard;
