452869f1de8e441f408d34deb0160b8e19f1dcfc
c7w <admin@cc7w.cf>
2022-04-11 01:20:23 +0800

    [SR.005.105] Modify merge request page css (#67)

--- src/components/rms/UIMerge.tsx
+++ src/components/rms/UIMerge.tsx
@@ -1,9 +1,38 @@
 import "./UIMerge.css";
 import React from "react";
+import moment from "moment";
 
-const SingleMergeEntry = () => {
+interface MergeEntryProps {
+  title: string;
+  description: string;
+  state: "merged" | "opened" | "closed";
+  mid: number;
+  associate_sr_id: number;
+  author_id: number;
+  author_time: number;
+}
+
+const SingleMergeEntry = (props: MergeEntryProps) => {
   const getBackgroundColor = () => {
-    return "#f6f6f6";
+    switch (props.state) {
+      case "closed":
+        return "#ffe5e5";
+      case "merged":
+        return "#e5ffe5";
+      case "opened":
+        return "#ffffff";
+    }
+  };
+
+  const getIssueState = () => {
+    switch (props.state) {
+      case "closed":
+        return "已关闭";
+      case "merged":
+        return "已合并";
+      case "opened":
+        return "正在审核";
+    }
   };
 
   return (
@@ -12,61 +41,27 @@ const SingleMergeEntry = () => {
       style={{ backgroundColor: getBackgroundColor() }}
     >
       <div className="issuable-main-info">
-        <div className="merge-request-title title">
-          <span className="merge-request-title-text js-onboarding-mr-item">
-            <a href="/undefined/frontend/-/merge_requests/63">
-              [SR.005.104] Bugfix: modify login regExp (#66)
-            </a>
-          </span>
+        <div className="merge-request-title">
+          <span style={{ fontWeight: "bold" }}>[{props.title}]</span>
+          &nbsp;&nbsp;
+          <span>{props.description}</span>
         </div>
         <div className="issuable-info">
-          <span className="issuable-reference">!63</span>
-          <span className="issuable-authored d-none d-sm-inline-block">
-            · opened{" "}
-            <time
-              className="js-timeago"
-              title=""
-              dateTime="2022-04-10T14:32:18Z"
-              data-toggle="tooltip"
-              data-placement="bottom"
-              data-container="body"
-              data-original-title="Apr 10, 2022 10:32pm GMT+0800"
-            >
-              1小时前
-            </time>
-            by{" "}
-            <a
-              className="author-link js-user-link  "
-              data-user-id="122"
-              data-username="2020010951"
-              data-name="高焕昂"
-              href="/2020010951"
-              title=""
-            >
-              <span className="author">高焕昂</span>
-            </a>
+          <span className="issuable-reference">!{props.mid}</span>
+          &nbsp;·&nbsp;
+          <span className="author">{props.author_id}</span>
+          &nbsp;·&nbsp;
+          <span className="authoredAt">
+            {moment(props.author_time * 1000).calendar()}
           </span>
         </div>
       </div>
 
       <div className="issuable-meta">
-        <div className="issuable-status d-none d-sm-inline-block">已合并</div>
+        <div className="issuable-status">{getIssueState()}</div>
 
         <div className="float-right issuable-updated-at d-none d-sm-inline-block">
-          <span>
-            更新于
-            <time
-              className="js-timeago merge_request_updated_ago"
-              title=""
-              dateTime="2022-04-10T14:36:59Z"
-              data-toggle="tooltip"
-              data-placement="bottom"
-              data-container="body"
-              data-original-title="Apr 10, 2022 10:36pm GMT+0800"
-            >
-              1小时前
-            </time>
-          </span>
+          <span>关联 {props.associate_sr_id}</span>
         </div>
       </div>
     </div>
@@ -87,11 +82,15 @@ const UIMerge = () => {
         合并情况查看
       </div>
       <hr style={{ width: "98%", margin: "1rem auto" }} />
-      <SingleMergeEntry />
-      <SingleMergeEntry />
-      <SingleMergeEntry />
-      <SingleMergeEntry />
-      <SingleMergeEntry />
+      <SingleMergeEntry
+        title={"SR.001.001"}
+        description={"Merge dev into master"}
+        associate_sr_id={1}
+        author_id={1}
+        mid={64}
+        state={"closed"}
+        author_time={1649836206}
+      />
     </div>
   );
 };
