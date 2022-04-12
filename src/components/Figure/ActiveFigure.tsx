383f17c0f6e37d31022f9b2e2416d060c6c8a9ba
qiancheng <qianc20@mails.tsinghua.edu.cn>
2022-04-12 12:01:22 +0800

    [SR.005.106.I] Finish the echarts ActiveFigureMR (#68)

--- src/components/Figure/ActiveFigure.tsx
+++ src/components/Figure/ActiveFigure.tsx
@@ -1,27 +1,48 @@
 import ReactEcharts from "echarts-for-react";
 import React, { Component } from "react";
-import "./IssueFigure.css";
+import "./ActiveFigure.css";
 
-const ActiveFigureMR = () => {
+const ActiveFigure = () => {
   const test =
     '[{"user":{"name":"c7w"},"mr":10,"line":3000},{"user":{"name":"wxy"},"mr":8,"line":1500},{"user":{"name":"hbx"},"mr":7,"line":2000}]';
   const data = JSON.parse(test);
   const MRdata: any = [];
+  const LineData: any = [];
   data.forEach((value: any) => {
     const name = value.user.name;
     const MR = value.mr;
-    const curData = {
+    const line = value.line;
+    const curDataMR = {
       value: MR,
       name: name,
     };
-    MRdata.push(curData);
+    const curDataLine = {
+      value: line,
+      name: name,
+    };
+    MRdata.push(curDataMR);
+    LineData.push(curDataLine);
   });
 
   const option = {
-    title: {
-      text: "开发工程师活跃度：MR查看",
-      left: "center",
-    },
+    title: [
+      {
+        text: "开发工程师活跃度分析",
+        left: "center",
+      },
+      {
+        subtext: "MR数量统计",
+        left: "25%",
+        top: "80%",
+        textAlign: "center",
+      },
+      {
+        subtext: "代码行数统计",
+        left: "75%",
+        top: "80%",
+        textAlign: "center",
+      },
+    ],
     tooltip: {
       trigger: "item",
     },
@@ -33,7 +54,7 @@ const ActiveFigureMR = () => {
       {
         name: "MR数量",
         type: "pie",
-        radius: "60%",
+        radius: "50%",
         data: MRdata,
         emphasis: {
           itemStyle: {
@@ -42,14 +63,35 @@ const ActiveFigureMR = () => {
             shadowColor: "rgba(0, 0, 0, 0.5)",
           },
         },
+        left: 0,
+        right: "50%",
+        top: 0,
+        bottom: 0,
+      },
+      {
+        name: "代码行数",
+        type: "pie",
+        radius: "50%",
+        data: LineData,
+        emphasis: {
+          itemStyle: {
+            shadowBlur: 10,
+            shadowOffsetX: 0,
+            shadowColor: "rgba(0, 0, 0, 0.5)",
+          },
+        },
+        left: "50%",
+        right: 0,
+        top: 0,
+        bottom: 0,
       },
     ],
   };
   return (
-    <div className={"issueChart"}>
+    <div className={"activeChart"}>
       <ReactEcharts option={option} />
     </div>
   );
 };
 
-export default ActiveFigureMR;
+export default ActiveFigure;
--- src/page/Router.tsx
+++ src/page/Router.tsx
@@ -23,7 +23,7 @@ import ProjectRequirementsReadonly from "./route/Project/ProjectRequirementsRead
 import ProjectMember from "./route/Project/ProjectMember";
 import Test from "../components/test"; // 测试接口
 import ProjectIteration from "./route/Project/ProjectIteration";
-import ActiveFigureMR from "../components/Figure/ActiveFigure";
+import ActiveFigure from "../components/Figure/ActiveFigure";
 
 const SiteRouter = () => {
   const testIR =
@@ -40,7 +40,7 @@ const SiteRouter = () => {
           <Route path="settings" element={<PersonalSetting />} />
           <Route path="project/:id/IRManager" element={<ProjectIR />} />
           <Route path="project/:id/SRManager" element={<ProjectSR />} />
-          <Route path="figure" element={<ActiveFigureMR />} />
+          <Route path="figure" element={<ActiveFigure />} />
           <Route
             path="project/:id/ServiceManager"
             element={<ProjectService />}
