62a61d869e23264600f48e4dd0944b3561d52243
qiancheng <qianc20@mails.tsinghua.edu.cn>
2022-04-12 10:47:19 +0800

    [SR.005.106.I] Finish the echarts IssueFigure (#68)

--- src/components/Figure/IssueFigure.tsx
+++ src/components/Figure/IssueFigure.tsx
@@ -1,4 +1,3 @@
-import echarts from "echarts";
 import ReactEcharts from "echarts-for-react";
 import React, { Component } from "react";
 import "./IssueFigure.css";
@@ -9,9 +8,30 @@ const IssueFigure = () => {
   const data = JSON.parse(test);
   const iterations = data.iterations;
   const allSR = data.all_sr_count;
+  let maxSR = 0;
+  allSR.forEach((value: number) => {
+    if (value > maxSR) {
+      maxSR = value;
+    }
+  });
   const issues = data.issues;
+  let maxIssue = 0;
+  allSR.forEach((value: number) => {
+    if (value > maxIssue) {
+      maxIssue = value;
+    }
+  });
+  const maxNum = maxIssue > maxSR ? maxIssue : maxSR;
+  const errors = [];
+  for (let i = 0; i < issues.length; i++) {
+    const curErr = ((issues[i] / allSR[i]) * 100).toFixed(2);
+    errors.push(curErr);
+  }
 
   const option = {
+    title: {
+      text: "需求交付质量分析",
+    },
     tooltip: {
       trigger: "axis",
       axisPointer: {
@@ -23,14 +43,14 @@ const IssueFigure = () => {
     },
     toolbox: {
       feature: {
-        dataView: { show: true, readOnly: false },
+        dataView: { show: false },
         magicType: { show: true, type: ["line", "bar"] },
         restore: { show: true },
-        saveAsImage: { show: true },
+        saveAsImage: { show: false },
       },
     },
     legend: {
-      data: ["Total SR", "Issues", "Error Rate"],
+      data: ["功能需求数量", "问题数量", "错误率"],
     },
     xAxis: [
       {
@@ -44,62 +64,56 @@ const IssueFigure = () => {
     yAxis: [
       {
         type: "value",
-        name: "Precipitation",
+        name: "数量",
         min: 0,
-        max: 260,
-        interval: 50,
+        max: maxNum + 2,
+        interval: 2,
         axisLabel: {
-          formatter: "{value} ml",
+          formatter: "{value}",
         },
       },
       {
         type: "value",
-        name: "Temperature",
+        name: "错误率",
         min: 0,
-        max: 25,
-        interval: 5,
+        max: 100,
+        interval: 20,
         axisLabel: {
-          formatter: "{value} °C",
+          formatter: "{value} %",
         },
       },
     ],
     series: [
       {
-        name: "Evaporation",
+        name: "功能需求数量",
         type: "bar",
         tooltip: {
           valueFormatter: function (value: any) {
-            return value + " ml";
+            return value;
           },
         },
-        data: [
-          2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 135.6, 162.2, 32.6, 20.0, 6.4, 3.3,
-        ],
+        data: allSR,
       },
       {
-        name: "Precipitation",
+        name: "问题数量",
         type: "bar",
         tooltip: {
           valueFormatter: function (value: any) {
-            return value + " ml";
+            return value;
           },
         },
-        data: [
-          2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3,
-        ],
+        data: issues,
       },
       {
-        name: "Temperature",
+        name: "错误率",
         type: "line",
         yAxisIndex: 1,
         tooltip: {
           valueFormatter: function (value: any) {
-            return value + " °C";
+            return value + "%";
           },
         },
-        data: [
-          2.0, 2.2, 3.3, 4.5, 6.3, 10.2, 20.3, 23.4, 23.0, 16.5, 12.0, 6.2,
-        ],
+        data: errors,
       },
     ],
   };
