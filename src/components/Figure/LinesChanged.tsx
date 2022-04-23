9f67d83d7479c568367f3cfde276c2eaafb4603e
qiancheng <qianc20@mails.tsinghua.edu.cn>
2022-04-23 15:02:00 +0800

    [SR.006.101.I] Continue drawing LinesChanged chart (#91)

--- src/components/Figure/LinesChanged.tsx
+++ src/components/Figure/LinesChanged.tsx
@@ -12,6 +12,49 @@ const LinesChanged = (props: LinesChangedProps) => {
   const test =
     '{"data":[{"commiter_name":"hxj","additions":10,"deletions":20},{"commiter_name":"glb","additions":20,"deletions":10},{"commiter_name":"wxy","additions":70,"deletions":0},{"commiter_name":"glb","additions":50,"deletions":30}]}';
   const data = JSON.parse(test).data;
+  const all_add: number[] = [];
+  const all_del: number[] = [];
+  const all_change: number[] = [];
+  let max_add = 0,
+    min_add = 10000,
+    max_del = 0,
+    min_del = 10000,
+    max_change = 0,
+    min_change = 10000;
+  data.forEach((item: any) => {
+    all_add.push(item.additions);
+    if (item.additions > max_add) max_add = item.additions;
+    if (item.additions < min_add) min_add = item.additions;
+    all_del.push(item.deletions);
+    if (item.deletions > max_del) max_del = item.deletions;
+    if (item.deletions < min_del) min_del = item.deletions;
+    const change = item.additions + item.deletions;
+    all_change.push(change);
+    if (change > max_change) max_change = change;
+    if (change < min_change) min_change = change;
+  });
+  max_add = Math.ceil(max_add / 50) * 50;
+  max_del = Math.ceil(max_del / 50) * 50;
+  max_change = Math.ceil(max_change / 50) * 50;
+  min_add = Math.floor(min_add / 50) * 50;
+  min_del = Math.floor(min_del / 50) * 50;
+  min_change = Math.floor(min_change / 50) * 50;
+  const add_data: any = [];
+  all_add.forEach((value: number) => {
+    value = Math.floor(value / 50) * 50 + 25;
+    let exist = false;
+    add_data.forEach((item: any) => {
+      if (item[0] === value) {
+        item[1] += 1;
+        exist = true;
+      }
+    });
+    if (exist === false) {
+      const new_item = [value, 1];
+      add_data.push(new_item);
+    }
+  });
+  
 
   const option = {
     title: {
@@ -25,9 +68,9 @@ const LinesChanged = (props: LinesChangedProps) => {
     },
     xAxis: {
       type: "value",
-      min: 10,
-      max: 100,
-      interval: 20,
+      min: Math.min(min_add, min_del),
+      max: max_change,
+      interval: 50,
       name: "\n\n行数",
     },
     yAxis: {
