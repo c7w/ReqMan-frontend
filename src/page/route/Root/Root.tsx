240eb859d584963eecfeb8c11f5237d262e1fe98
何秉翔 <2020010944@secoder.net>
2022-04-27 14:54:54 +0800

    [SR.006.109.I] Add title and slogan style (#99)

--- src/page/route/Root/Root.tsx
+++ src/page/route/Root/Root.tsx
@@ -15,6 +15,8 @@ import { useDispatch } from "react-redux";
 import { updateUserStore } from "../../../store/slices/UserSlice";
 import { push } from "redux-first-history";
 import { Progress } from "rsup-progress";
+import Texty from "rc-texty";
+import TweenOne from "rc-tween-one";
 
 const Root = () => {
   const dispatcher = useDispatch();
@@ -48,6 +50,9 @@ const Root = () => {
           // setTimeout(() => {
           //   dispatcher(push("/dashboard")); // Use this way to redirect
           // }, 1000);
+          // setTimeout(() => {
+          //   dispatcher(push("/login")); // Use this way to redirect
+          // }, 1000);
         } else {
           immediateToast("success", {
             title: "连接成功",
@@ -71,17 +76,145 @@ const Root = () => {
   // useEffect Hook: Would be executed when the variables in the dependence list are initialized or changed value.
   // If you define an empty list as variable dependence, this function would just execute for once, just like didMount().
 
+  const getInterval = (e: any) => {
+    switch (e.index) {
+      case 0:
+        return 0;
+      case 1:
+        return 0;
+      case 2:
+        return 150;
+      case 3:
+      case 4:
+      case 5:
+      case 6:
+        return 150 + 450 + (e.index - 2) * 10;
+      default:
+        return 150 + 450 + (e.index - 6) * 150;
+    }
+  };
+  const getEnter = (e: any) => {
+    const t = {
+      opacity: 0,
+      scale: 0.8,
+      y: "-100%",
+    };
+    if (e.index >= 2 && e.index <= 6) {
+      return { ...t, y: "-30%", duration: 150 };
+    }
+    return t;
+  };
+
+  const getSplit = (e: any) => {
+    const t = e.split(" ");
+    const c: any = [];
+    t.forEach((str: any, i: any) => {
+      c.push(<span key={`${str}-${i}`}>{str}</span>);
+      if (i < t.length - 1) {
+        c.push(<span key={` -${i}`}> </span>);
+      }
+    });
+    return c;
+  };
+
   return (
     <div className={"root-screen"}>
+      <div className="combined">
+        <div className="combined-shape">
+          <div className="shape-left">
+            <TweenOne
+              animation={[
+                {
+                  x: "20vw",
+                  type: "from",
+                  ease: "easeInOutQuint",
+                  duration: 600,
+                },
+                {
+                  x: "-20vw",
+                  ease: "easeInOutQuart",
+                  duration: 450,
+                  delay: -150,
+                },
+              ]}
+            />
+          </div>
+          <div className="shape-right">
+            <TweenOne
+              animation={[
+                {
+                  x: "-20vw",
+                  type: "from",
+                  ease: "easeInOutQuint",
+                  duration: 600,
+                },
+                {
+                  x: "20vw",
+                  ease: "easeInOutQuart",
+                  duration: 450,
+                  delay: -150,
+                },
+              ]}
+            />
+          </div>
+        </div>
+        <Texty
+          className="title"
+          type="mask-top"
+          delay={400}
+          enter={getEnter}
+          interval={getInterval}
+          component={TweenOne}
+          componentProps={{
+            animation: [
+              { x: 200, type: "set" },
+              { x: 100, delay: 500, duration: 450 },
+              {
+                ease: "easeOutQuart",
+                duration: 300,
+                x: 0,
+              },
+              {
+                letterSpacing: 0,
+                delay: -300,
+                scale: 0.9,
+                ease: "easeInOutQuint",
+                duration: 1000,
+              },
+              {
+                scale: 1,
+                width: "100%",
+                delay: -300,
+                duration: 1000,
+                ease: "easeInOutQuint",
+              },
+            ],
+          }}
+        >
+          ReqMan
+        </Texty>
+        <TweenOne
+          className="combined-bar"
+          animation={{
+            delay: 2000,
+            width: 0,
+            x: "10vw",
+            type: "from",
+            ease: "easeInOutExpo",
+          }}
+        />
+        <Texty
+          className="content"
+          type="bottom"
+          split={getSplit}
+          delay={2200}
+          interval={30}
+        >
+          Your Requirement Management Servant
+        </Texty>
+      </div>
       <div id="loading-progress">
         <span id="loading-progress-thumb" />
-      </div>
-      <div className={"root-proj-name"}>
-        <img src={logo} width={800} height={400} alt="logo" />
-      </div>
-      <div className={"root-proj-slogan"}>
-        Your <span style={{ fontWeight: "bold" }}>Req</span>uirement{" "}
-        <span style={{ fontWeight: "bold" }}>Man</span>agement Servant
       </div>
       <div className={"root-loading"}>
         <Loading3QuartersOutlined
